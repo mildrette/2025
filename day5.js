2cc27909aeaefe0895cdc5dfe1cf19f5c24323d0
mildrette <mildredfonka1@gmail.com>
2025-01-06 14:38:04 +0100

    added function

--- day5.js
+++ day5.js
@@ -6,7 +6,28 @@ const replicate = readline.createInterface({
     output: process.stdout
 })
 
-replicate.question('hey there, what are you doing?? ', (answer) => {console.log("oh realy, that is nice")
+replicate.question('hey there, what are you doing?? ', (answer) => {
+    console.log("oh realy, that is nice")
     replicate.close();
+
+    const quiz = readline.createInterface({
+        input: process.stdin,
+        output: process.stdout
+    })
+
+    function askQuestion(){
+        quiz.question(" emmh please what is 8 - 5", (answer) => {
+            if(parseInt(answer) === 3) {
+                console.log("not bad for someone like you");
+                quiz.close();
+            } else {
+                console.log('try again and please think before answeing');
+                askQuestion();
+            }
+        });
+
+    }
+    askQuestion();
  });
+
 
